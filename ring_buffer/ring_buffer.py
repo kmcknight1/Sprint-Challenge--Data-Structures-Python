aa5c93e7aff83b4b1722e619cd73497968dc8eb4
kmcknight1 <katiemcknight1991@gmail.com>
2020-04-24 11:27:16 -0500

    completes ring buffer and stretch

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,25 +8,53 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # if length of the list is less than capacity, add item to tail and make current the head
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        else:
+            # if current has a next, set that spot to the new item, make the next node the new current
+            if self.current.next:
+                self.current.value = item
+                self.current = self.current.next
+            else:
+                # if current is tail, set that spot to the new item, make the head the new current
+                self.current.value = item
+                self.current = self.storage.head
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        # start at head, loop through and append each node.value to the list (making node = next to move through)
+        node = self.storage.head
+        while node:
+            list_buffer_contents.append(node.value)
+            node = node.next
 
         return list_buffer_contents
+
 
 # ----------------Stretch Goal-------------------
 
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        pass
+        # storage is an array with available slots based on capacity
+        self.storage = [None] * capacity
+        self.current_index = 0
+        self.capacity = capacity
 
     def append(self, item):
-        pass
+        # set the item in place of the current
+        self.storage[self.current_index] = item
+
+        # move the current_index to the beginning of the list
+        if self.current_index == self.capacity-1:
+            self.current_index = 0
+        # move the current_index up one, to be the new item
+        else:
+            self.current_index += 1
 
     def get(self):
-        pass
+        return [item for item in self.storage if item]
